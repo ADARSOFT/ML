115e0d367a4442785e53d547a06c99419219314b
Adarsoft <50028092+ADARSOFT@users.noreply.github.com>
2019-10-06 15:56:16 +0200

    Dodata je predikcija za KMeans. Pripremljeni podaci za Silhouete index task.

--- Algorithms/Kmeans/Code/kmeans_final.py
+++ Algorithms/Kmeans/Code/kmeans_final.py
@@ -7,23 +7,26 @@ import kmeans_lib
 
 np.set_printoptions(formatter={'float_kind':lambda x: "%.3f" % x})
 pd.set_option('chop_threshold',0.01)
+
 #%% LOAD DATA
 data = pd.read_csv('../../../Data/boston.csv').set_index('country') # ZA BOSTON NE TREBA COUNTRY
-k = 4
 m,n = data.shape
 #%% NORMALIZATION https://en.wikipedia.org/wiki/Standard_score
 data_mean = data.mean()
 data_std = data.std()
 data = (data-data_mean)/data_std
+boston_weights = [1,2,1,1,1,1,1,1,1,1,1,1,1,1]
+life_weights = [1,2,1,1,1]
 
-def FindOutermostCentroids(k_number):
+#%% HELP method
+def FindOutermostCentroids(p_k_number):
 	current_centroid = data.sample(1)
 	centroids = pd.DataFrame()
 	centroids = centroids.append(current_centroid)
 	distances_k = pd.DataFrame() 
 	data_with_no_c = pd.DataFrame()
 
-	for i in range(k_number-1):
+	for i in range(p_k_number-1):
 		data_with_no_c = data[~data.isin(centroids)].dropna()
 		for j in range(len(data_with_no_c)):
 			distances_k = distances_k.append((abs(data_with_no_c.iloc[j:j+1,:].reset_index(drop=True) - current_centroid.reset_index(drop=True))), ignore_index=True)
@@ -33,27 +36,28 @@ def FindOutermostCentroids(k_number):
 		distances_k = distances_k.iloc[0:0]
 	
 	return centroids
-#%% ALGORITHM
-def KMeans_experiment(experiment_numbers, feature_weights, max_iteration = 100, dist_measure_type = 'euclidian'):
-	res = kmeans_lib.KMeansExperimentResponse([], np.zeros(experiment_numbers))
+
+#%% ALGORITHM fit
+def KMeans_Fit(p_k, p_experiment_numbers, p_feature_weights, p_max_iteration = 100, p_dist_measure_type = 'euclidian'):
+	res = kmeans_lib.KMeansExperimentResponse([], np.zeros(p_experiment_numbers))
 	res.CentroidHistory.clear()
 	res.BestCentroid.clear()
-	for expn in range(experiment_numbers):
-		centroids = FindOutermostCentroids(k).reset_index(drop=True)
-		res.CentroidHistory.append(centroids.values)
+	for expn in range(p_experiment_numbers):
+		centroids = FindOutermostCentroids(p_k).reset_index(drop=True)
+		res.CentroidHistory.append(centroids)
 		assign = np.zeros((m,1))
 		old_quality = float('inf')
-		for it in range(max_iteration):
-			quality = np.zeros(k) 
+		for it in range(p_max_iteration):
+			quality = np.zeros(p_k) 
 			for j in range(m):
 				point = data.iloc[j]
 				dist = []
-				if dist_measure_type == 'city-block':
-					dist = abs(point-centroids).mul(feature_weights).sum(axis=1)
+				if p_dist_measure_type == 'city-block':
+					dist = abs(point-centroids).mul(p_feature_weights).sum(axis=1)
 				else: 
-					dist = ((point-centroids)**2).mul(feature_weights).sum(axis=1).apply(mt.sqrt)
+					dist = ((point-centroids)**2).mul(p_feature_weights).sum(axis=1).apply(mt.sqrt)
 				assign[j] = np.argmin(dist)	
-			for c in range(k):
+			for c in range(p_k):
 				subset = data[assign==c]
 				centroids.loc[c] = subset.mean()
 				quality[c] = subset.var().sum() * len(subset)
@@ -64,10 +68,19 @@ def KMeans_experiment(experiment_numbers, feature_weights, max_iteration = 100, 
 		res.QualityHistory[expn] = quality.sum()
 	res.BestCentroid = res.CentroidHistory[np.argmin(res.QualityHistory)]
 	return res	
-#%% Call function
-res = kmeans_lib.KMeansExperimentResponse([], np.zeros(5))
-res = KMeans_experiment(5, [1,2,1,1,1,1,1,1,1,1,1,1,1,1])
-res.CentroidHistory
-res.QualityHistory
-res.BestCentroid 
-#%% Objects
+
+#%% Predict kmeans
+def KMeans_Predict(p_centroids, p_data):
+	m_m, m_n = p_data.shape
+	m_assign = np.zeros((m_m,1))
+	for j in range(m_m):
+		m_assign[j] = np.argmin(((p_data.iloc[j]-p_centroids)**2).sum(axis=1).apply(mt.sqrt))
+	p_data['Cluster'] = m_assign
+	return p_data	
+	
+#%% Algorithm usage
+res = kmeans_lib.KMeansExperimentResponse([], np.zeros(7))
+res = KMeans_Fit(5, 7, boston_weights)
+predict_model = KMeans_Predict(res.BestCentroid, data)
+predict_model.groupby(['Cluster']).count().iloc[:,-1]
+
