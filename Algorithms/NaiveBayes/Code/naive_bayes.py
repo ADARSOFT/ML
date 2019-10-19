9e4a6a8ae6580bfa6c82c76bf40ee73b27b48533
Adarsoft <50028092+ADARSOFT@users.noreply.github.com>
2019-10-19 17:30:13 +0200

    Finish Underflow problem for NaiveBayes algorithm. Start with smoothing (additive).

--- Algorithms/NaiveBayes/Code/naive_bayes.py
+++ Algorithms/NaiveBayes/Code/naive_bayes.py
@@ -1,49 +1,40 @@
 import pandas as pd
 import numpy as np
-import math 
-import sys
-import copy
-import extmath
 
 #%%  UCENJE
-def learn(podaci, klasa):
+def learn(data, outputClass):
 	model = {}
-	apriori = podaci[klasa].value_counts()
+	apriori = data[outputClass].value_counts()
 	apriori = apriori / apriori.sum()
 	model['apriori'] = apriori
 
-	for atribut in podaci.drop(klasa, axis=1).columns:
-		mat_kont = pd.crosstab(podaci[atribut],podaci[klasa])
+	for atribut in data.drop(outputClass, axis=1).columns:
+		mat_kont = pd.crosstab(data[atribut],data[outputClass])
 		mat_kont = mat_kont.div(mat_kont.sum())
 		model[atribut] = mat_kont
 
 	return model
 #%% PREDVIDJANJE
 def predict(model, slucaj):
-	predvidjanje = {}
-	#slucaj = point 
-	for klasa in model['apriori'].index:
-		#klasa = 'ne'
-		verovatnoca = 1
+	predictResponse = {}
+	for outputClass in model['apriori'].index:
+		probability = 1
 		for atribut in model:
 			if atribut == 'apriori':
-				verovatnoca = verovatnoca * model['apriori'][klasa]
+				probability = probability * model['apriori'][outputClass]
 			else:
-				# Pomnozi prethodni P sa P iz modela za slucaj (njegov atribut)
-				caseProb = model[atribut][klasa][slucaj[atribut]]
-				verovatnoca = verovatnoca * np.log(np.sum(np.exp(caseProb)))
-				if verovatnoca < 0:
-					print('Verovatnoca manja od 0: {}'.format(verovatnoca))
-
-		predvidjanje[klasa]=verovatnoca
-	return predvidjanje
+				conditionProb = model[atribut][outputClass][slucaj[atribut]]
+				exponentValue = np.exp(conditionProb)
+				logarithmOfSumOfExponent = np.log(exponentValue)
+				probability = probability * logarithmOfSumOfExponent
+		predictResponse[outputClass]=probability
+	return predictResponse
 #%% KORISCENJE
 data = pd.read_csv('prehlada.csv')
 model = learn(data,'Prehlada')
 
 data_new = pd.read_csv('prehlada_novi.csv')
 for i in range(len(data_new)):
-	#i = 0
 	point = data_new.loc[i]
 	prediction = predict(model, point)
 	data_new.loc[i,'prediction'] = max(prediction, key=lambda x: prediction[x])
@@ -51,9 +42,13 @@ for i in range(len(data_new)):
 		data_new.loc[i,'klasa='+klasa] = prediction[klasa]
 
 print(data_new)
+
+# Underflow with log and exp solution for the problem.
 # https://stackoverflow.com/questions/33434032/avoid-underflow-using-exp-and-minimum-positive-float128-in-numpy
 # https://www.youtube.com/watch?v=-RVM21Voo7Q
-print(np.log(0.444))
-print(np.log(3))
-print(np.log(-9))
-print(np.log(4))
+# Laplace smoothing (additive)
+# https://medium.com/syncedreview/applying-multinomial-naive-bayes-to-nlp-problems-a-practical-explanation-4f5271768ebf
+# https://en.wikipedia.org/wiki/Additive_smoothing
+
+
+
