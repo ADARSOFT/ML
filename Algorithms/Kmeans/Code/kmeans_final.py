ff3d9bae2f7b13940739db0a0cb96ed20d2164ec
Adarsoft <50028092+ADARSOFT@users.noreply.github.com>
2019-12-10 22:39:08 +0100

    Kmeans bug fixing.

--- Algorithms/Kmeans/Code/kmeans_final.py
+++ Algorithms/Kmeans/Code/kmeans_final.py
@@ -2,65 +2,89 @@ import pandas as pd
 import numpy as np
 import math as mt
 import kmeans_lib
-from sklearn.metrics import silhouette_score
-
+from sklearn.metrics import silhouette_score as sklearn_silhouette_score
+from copy import copy
 np.set_printoptions(formatter={'float_kind':lambda x: "%.3f" % x})
 pd.set_option('chop_threshold',0.01)
 
-#%% HELP method
-def FindOutermostCentroids(p_k_number):
-	current_centroid = data.sample(1)
+#%% Helper methods
+def findInstancesThatAreNotCentroids(isSingleResponse, data, centroids):
+	if isSingleResponse:
+		# return random single instance that isn't centroid
+		return data[~data.isin(centroids)].sample(1)
+	else:
+		# return all instances that aren't centroids
+		return data[~data.isin(centroids)].dropna()
+
+#%% TASK 4 - This method we use when we initialize clusters (already defined number of clusters)
+def findOutermostCentroids(p_k_number):
 	centroids = pd.DataFrame()
+	current_centroid = findInstancesThatAreNotCentroids(True, data, centroids)
 	centroids = centroids.append(current_centroid)
 	distances_k = pd.DataFrame() 
 	data_with_no_c = pd.DataFrame()
 
 	for i in range(p_k_number-1):
-		#data_with_no_c = data_with_no_c.iloc[0:0]
-		data_with_no_c = data[~data.isin(centroids)].dropna()
-		for j in range(len(data_with_no_c)):
-			distances_k = distances_k.append((abs(data_with_no_c.iloc[j:j+1,:].reset_index(drop=True) - current_centroid.reset_index(drop=True))), ignore_index=True)
+		# Get only data without centroids
+		data_with_no_c = findInstancesThatAreNotCentroids(False, data, centroids)		
+		# Calculate distances from intances to current cluster
+		distances_k = abs(data_with_no_c.sub(current_centroid.values.tolist()[0], axis = 'columns'))
+		# Take index of outermost instance
 		idx = np.argmax(distances_k.sum(axis = 1))
+		# Take outermost centroid (instance) from training dataset by index
 		current_centroid = data.iloc[idx:idx+1,:]
+		# Add centroid to output list
 		centroids = centroids.append(current_centroid)
+		# Reset distances
 		distances_k = distances_k.iloc[0:0]
 	
 	return centroids
+#%% # TASK 0 AND 1 AND 2 (calculate weignted instances with optional measure type (euclidian, city-block)) 
+def calculateWeightedDistance(dist_measure_type, point, centroids, feature_weights):
+	if dist_measure_type == 'city-block':
+		return abs(point-centroids).mul(feature_weights).sum(axis=1)
+	else:
+		return ((point-centroids)**2).mul(feature_weights).sum(axis=1).apply(mt.sqrt)
 
 #%% ALGORITHM fit
-def KMeans_Fit(p_k, p_experiment_numbers, p_feature_weights, p_max_iteration = 100, p_dist_measure_type = 'euclidian'):
-	res = kmeans_lib.KMeansExperimentResponse([], np.zeros(p_experiment_numbers))
+def fit(total_clusters_number, total_experiments_number, p_feature_weights, p_max_iteration = 100, p_dist_measure_type = 'euclidian'):
+	res = kmeans_lib.KMeansExperimentResponse([], np.zeros(total_experiments_number))
 	res.CentroidHistory.clear()
 	res.BestCentroid.clear()
-	for expn in range(p_experiment_numbers):
-		centroids = FindOutermostCentroids(p_k).reset_index(drop=True)
+	# TASK 3 (restart algorithm N times and choose best centroid quality)
+	for expn in range(total_experiments_number):
+		# Cluster initialization, find outermost clusters on first step 
+		centroids = findOutermostCentroids(total_clusters_number).reset_index(drop=True)
 		res.CentroidHistory.append(centroids)
 		assign = np.zeros((m,1))
 		old_quality = float('inf')
 		for it in range(p_max_iteration):
-			quality = np.zeros(p_k) 
+			quality = np.zeros(total_clusters_number)
+		
+		    # Calculate witch cluster instance belong (with optinal distances metrics)
 			for j in range(m):
-				point = data.iloc[j]
-				dist = []
-				if p_dist_measure_type == 'city-block':
-					dist = abs(point-centroids).mul(p_feature_weights).sum(axis=1)
-				else: 
-					dist = ((point-centroids)**2).mul(p_feature_weights).sum(axis=1).apply(mt.sqrt)
-				assign[j] = np.argmin(dist)	
-			for c in range(p_k):
+				dist = calculateWeightedDistance(p_dist_measure_type, data.iloc[j], centroids, p_feature_weights)
+				# Assing instance
+				assign[j] = np.argmin(dist)
+				
+			# Calculate new centroids (mean of subset)
+			for c in range(total_clusters_number):
 				subset = data[assign==c]
 				centroids.loc[c] = subset.mean()
 				quality[c] = subset.var().sum() * len(subset)
+				
 			print('Redni broj iteracije: {}'.format(it))
 			if np.array_equal(quality, old_quality): break
 			old_quality = quality
+			
 		print('Experiment number: {} Quality: {}.'.format(expn, quality.sum()))
 		res.QualityHistory[expn] = quality.sum()
+		
 	res.BestCentroid = res.CentroidHistory[np.argmin(res.QualityHistory)]
-	return res	
+	return res
 
 #%% Predict kmeans
-def KMeans_Predict(p_centroids, p_data):
+def predict(p_centroids, p_data):
 	m_m, m_n = p_data.shape
 	m_assign = np.zeros((m_m,1))
 	for j in range(m_m):
@@ -68,28 +92,24 @@ def KMeans_Predict(p_centroids, p_data):
 	p_data['Cluster'] = m_assign
 	return p_data
 
-#%% Silhouette score calculation function
+#%% Scalar mean distance
 def GetScalarMeanDistance(p_cluster_data, p_cluster_point):
-	#print('-------------------------------------------------')
-	#print('Cluster data:')
-	#print(p_cluster_data)
-	#print('Cluster point')
-	#print(p_cluster_point)
-	#print('Cluster poin minus cluster data')
-	#print(p_cluster_point - p_cluster_data)
-	#print('Mean(cluster_point-cluster_data)')
-	#print(np.mean(p_cluster_point - p_cluster_data))
 	resp = np.mean(((p_cluster_point - p_cluster_data)**2).sum(axis=1).apply(mt.sqrt))
-	#print('Mean of Mean: {}'.format(resp))
 	return resp
 
-def SilhouetteScore(p_k, p_labeled_data, p_centroids):
-	
-	# Prepare, add new columns
-	#m_labeled_data = predict_model
-	#m_centroids =  res.BestCentroid
-	m_labeled_data = p_labeled_data
-	m_centroids =  p_centroids
+#%% CalculateSilhouette index value
+def silhouetteIndexCalculation(a,b):	
+	if a < b:
+		return float(1- a/b)
+	elif a > b:
+		return float(b/a-1)
+	elif a == b:
+		return 0
+
+#%% TASK 5. Silhouette score calculation function
+def silhouetteScore(p_k, labeled_data, centroids):
+	m_labeled_data = copy(labeled_data)
+	m_centroids =  copy(centroids)
 	m_labeled_data['IntraClusterMean'] = float(0)
 	m_labeled_data['NearestClusterMean'] = float(0)
 	m_labeled_data['SilhouetteIndex'] = float(0)
@@ -98,53 +118,50 @@ def SilhouetteScore(p_k, p_labeled_data, p_centroids):
 	c_row_number, c_column_number = m_centroids.shape
 	ld_row_number,ld_column_number = m_labeled_data.shape
 	
-	# Dodeljujem najblize centroide
+	# Assign nearest centroids
 	for i in range(c_row_number):
 		m_centroids['NearestCluster'][i] = np.argmin(((m_centroids.iloc[i] - m_centroids[~ m_centroids.index.isin([i])])**2).sum(axis=1).apply(mt.sqrt))
-	
-	# Calculate SilhuetteIndex per observation by formula (a-b) / max(a,b)	
+
 	for i in range(ld_row_number):
-		#i = 0
+		
+		# Extraxt sigle instance into point variable 
 		point = m_labeled_data.iloc[i, :-4]
+		
+		# Calculate self cluster mean distance
 		self_cluster_data = m_labeled_data.loc[m_labeled_data['Cluster'] == m_labeled_data.iloc[i]['Cluster']].iloc[:,:-4]
 		a = GetScalarMeanDistance(self_cluster_data[~self_cluster_data.index.isin([i])], point)
 		m_labeled_data['IntraClusterMean'][i] = a
+		
+		# Calculate nearest cluster mean distance 
 		current_centroid = int(m_labeled_data.iloc[i]['Cluster'])
 		nearest_centroid = int(m_centroids.iloc[current_centroid]['NearestCluster'])
 		b = GetScalarMeanDistance(m_labeled_data.loc[m_labeled_data['Cluster'] == nearest_centroid].iloc[:,:-4], point)
 		m_labeled_data['NearestClusterMean'][i] = b
 		
-		if a < b:
-			m_labeled_data['SilhouetteIndex'][i] = float(1- a/b)
-		elif a > b:
-			m_labeled_data['SilhouetteIndex'][i] = float(b/a-1)
-		elif a == b:
-			m_labeled_data['SilhouetteIndex'][i] = 0
+		# Calculate solhouette index
+		m_labeled_data['SilhouetteIndex'][i] = silhouetteIndexCalculation(a,b)
 	
 	return np.mean(m_labeled_data[['SilhouetteIndex']])
 
 #%% LOAD DATA and params configuration
-data = pd.read_csv('../../../Data/boston.csv')#.set_index('country') # ZA BOSTON NE TREBA COUNTRY
-m,n = data.shape
-data_mean = data.mean()
-data_std = data.std()
-data = (data-data_mean)/data_std # Standard_score
 boston_weights = [1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 life_weights = [1,1,1,1,1]
 visina_tezina_weights = [1,1]
+alphas = visina_tezina_weights
+experiments = 4
+silhoueteScoreHistory = []
+init_data = pd.read_csv('../../../Data/visina_tezina.csv')#.set_index('country') # Just for life csv dataset
 
-#%% Algorithm usage
-
-res = kmeans_lib.KMeansExperimentResponse([], np.zeros(7))
-
-
-res = KMeans_Fit(3, 7, boston_weights)
-predict_model = KMeans_Predict(res.BestCentroid, data)
-
-score = silhouette_score(predict_model.iloc[:,0:14], predict_model.iloc[:,-1], metric='euclidean')
-
-# predict_model.groupby(['Cluster']).count().iloc[:,-1]
-silhoueteScore = SilhouetteScore(3, predict_model, res.BestCentroid)
-silhoueteScore
-
-# 5. -0.378873, 2. -0.673033, 3. -0.354913 4. -0.29497 5. -0.326227 10. -0.544737
+for i in range(10):
+	clusters = i + 2
+	data = copy(init_data)
+	m,n = data.shape
+	data_mean = data.mean()
+	data_std = data.std()
+	data = (data-data_mean)/data_std # Standard_score
+	res = kmeans_lib.KMeansExperimentResponse([], np.zeros(experiments))
+	res = fit(clusters, experiments, alphas)
+	predict_model = predict(res.BestCentroid, data)
+	score = sklearn_silhouette_score(predict_model.iloc[:,0:n], predict_model.iloc[:,-1], metric='euclidean')
+	silhoueteScore = silhouetteScore(clusters, predict_model, res.BestCentroid)
+	silhoueteScoreHistory.append([clusters, score, silhoueteScore])
