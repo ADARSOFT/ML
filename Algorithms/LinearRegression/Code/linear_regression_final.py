b1e8b6c81c0b2b99beb7ecab269837615665e205
Adarsoft <50028092+ADARSOFT@users.noreply.github.com>
2019-12-15 21:51:00 +0100

    Resolve bug for Ridge regression (linear regression alg).

--- Algorithms/LinearRegression/Code/linear_regression_final.py
+++ Algorithms/LinearRegression/Code/linear_regression_final.py
@@ -29,19 +29,72 @@ X = X.values
 # Calculate input data shape
 n,m = X.shape
 
-#%% INITIALIZATION
 # Return random floats in the half-open interval [0.0, 1.0) for Slope's. 
 # We use array length as X number of columns (m).
 theta = np.random.random((1,m))
 # Define learning rate for gradient descent
 #learning_rate = 0.0004
 # Define maximum iteration number
-max_iteration = 1000
+max_iteration = 2000
 # Define gradient min norm
 min_grad_norm = 0.01
 
+#%% Calculate cost function
+def calcCostMethod1(theta_p, X_p, y_p):
+	m = len(y_p)
+	predictions = X_p.dot(theta_p)
+	cost = (1/2*m) * np.sum(np.square(predictions-y_p))
+	return cost
+
+# Normalized Residual sum of squeres
+def calcCostMethod2(residualsT_p, residuals_p, n_p):
+	cost = np.dot(residualsT_p, residuals_p) / n_p
+	return cost[0][0]
+
+#%% Generate random sample
+def generateRandomSample(X, y):
+	rnd_idx = np.random.randint(0, n)
+	X_i = X[rnd_idx, :].reshape(1, X.shape[1])
+	y_i = y[rnd_idx].reshape(1,1)
+	return X_i, y_i
+
+#%% Ridge regression calculation 
+def calculateRidgeRegressionPenalty(theta, lambda_penalty):
+	# Squere slope params, except intercept
+	slope_squered = (theta**2)[0][1:]
+	
+	# Variant 1 - set intercept to zero
+	intercept = [0]
+	# Variant 2 Get only intercept
+	#intercept = theta[0][0:1]
+	
+	# Penalize slope but not intercept, and after that concatenate
+	response = np.concatenate([intercept, lambda_penalty * slope_squered])
+	return response
+
+#%% Calculate learning rate Exponential decay by epoch
+def exp_decay(epoch):
+	initial_rate = 0.1
+	k =0.2
+	l_rate = initial_rate * mt.exp(-k*epoch)
+	return l_rate 
+
+#%% Calculate step decay
+def step_decay(epoch):
+	initial_lrate = 0.1
+	drop = 0.1
+	epochs_drop = 10
+	lrate = initial_lrate * mt.pow(drop, mt.floor((1+epoch)/epochs_drop))
+	return lrate
+
+#%% Visualisation
+def showPlot(x_iteration, x_mse_per_iteration):
+		plt.plot(x_iteration, x_mse_per_iteration)
+		plt.xlabel('No. of iterations')
+		plt.ylabel('Mean squered error')
+
 #%% Create function with params 
-def GD_LinearRegression(X, theta, y, max_iteration, min_grad_norm, n, lambda_penalty = 1, lr_scheduler_type = "exp"):
+def fit(X, theta, y, max_iteration, min_grad_norm, n, lambda_penalty = 1, lr_scheduler_type = "exp"):
 	
 	x_iteration = []
 	x_mse_per_iteration = []
@@ -71,15 +124,16 @@ def GD_LinearRegression(X, theta, y, max_iteration, min_grad_norm, n, lambda_pen
 			residuals = (pred - y_i)
 
 			# *Ridge regression calculation*
-			# SSR + (lambda * theta **2) #(Note: not penalize intercept)
+			# lambda*theta**2 (Note: not penalize intercept)
 			penalty_vector = calculateRidgeRegressionPenalty(theta, lambda_penalty)
 		
-			# Gradient descent calculation
+			# Gradient descent calculation (Mean feature error Lost function)
 			gradient_vector = (np.dot(residuals.T, X_i) + penalty_vector) / n
 		
 			# Calculate step_size by multiplying learning_rate and gradient_vector (gradient_vector => data descent to minimum).
 			step_size = learning_rate * gradient_vector
-			# Calculate new theta - Loss function parameters (old w - step_size) * learning rate
+			
+			# Calculate new theta (old w - step_size) * learning rate
 			theta = theta - step_size
 		
 			# Calculate sum of absolute gradient_vector values
@@ -97,59 +151,12 @@ def GD_LinearRegression(X, theta, y, max_iteration, min_grad_norm, n, lambda_pen
 
 	showPlot(x_iteration, x_mse_per_iteration)
 
-#%% Calculate cost function
-def calcCostMethod1(theta_p, X_p, y_p):
-	m = len(y_p)
-	predictions = X_p.dot(theta_p)
-	cost = (1/2*m) * np.sum(np.square(predictions-y_p))
-	return cost
-
-# Normalized Residual sum of squeres
-def calcCostMethod2(residualsT_p, residuals_p, n_p):
-	cost = np.dot(residualsT_p, residuals_p) / n_p
-	return cost[0][0]
-
-#%% Generate random sample
-def generateRandomSample(X, y):
-	rnd_idx = np.random.randint(0, n)
-	X_i = X[rnd_idx, :].reshape(1, X.shape[1])
-	y_i = y[rnd_idx].reshape(1,1)
-	return X_i, y_i
-
 #%% Predict method
 def predict(features, theta):
 	return np.dot(features, theta)
 
-#%% Ridge regression calculation 
-def calculateRidgeRegressionPenalty(theta, lambda_penalty):
-	theta_squered = theta**2
-	slope_squered = theta_squered[0][1:]
-	intercept_squered = theta_squered[0][0:1]
-	return np.concatenate([intercept_squered, lambda_penalty * slope_squered])
-
-#%% Calculate learning rate Exponential decay by epoch
-def exp_decay(epoch):
-	initial_rate = 0.1
-	k =0.2
-	l_rate = initial_rate * mt.exp(-k*epoch)
-	return l_rate 
-
-#%% Calculate step decay
-def step_decay(epoch):
-	initial_lrate = 0.1
-	drop = 0.1
-	epochs_drop = 10
-	lrate = initial_lrate * mt.pow(drop, mt.floor((1+epoch)/epochs_drop))
-	return lrate
-
-#%% Visualisation
-def showPlot(x_iteration, x_mse_per_iteration):
-		plt.plot(x_iteration, x_mse_per_iteration)
-		plt.xlabel('No. of iterations')
-		plt.ylabel('Mean squered error')
-	
 #%% Use linear regression with above function 
-GD_LinearRegression(X, theta, y, max_iteration, min_grad_norm, n, 1)
+fit(X, theta, y, max_iteration, min_grad_norm, n, 1)
 
 #%% PREDICT
 data_new = pd.read_csv('house_new.csv')
@@ -157,3 +164,5 @@ data_new = (data_new-X_mean)/X_std
 data_new.insert(0, 'X0', 1)
 prediction = predict(data_new.values, theta.T)
 print(prediction)
+
+# http://fa.bianp.net/teaching/2018/eecs227at/stochastic_gradient.html
